ce9273bf7f8253db92ad5b1da8101c7756a26cab
Daniel Berger <djberg96@gmail.com>
2015-01-10 16:00:35 -0700

    Declare WaitForSingleObject as blocking.

--- lib/win32/ipc.rb
+++ lib/win32/ipc.rb
@@ -14,13 +14,13 @@ module Win32
     ffi_lib :kernel32
 
     attach_function :CloseHandle, [:handle], :bool
-    attach_function :WaitForSingleObject, [:handle, :dword], :dword
+    attach_function :WaitForSingleObject, [:handle, :dword], :dword, :blocking => true
     attach_function :WaitForMultipleObjects, [:dword, :pointer, :bool, :dword], :dword, :blocking => true
 
     private_class_method :CloseHandle, :WaitForSingleObject, :WaitForMultipleObjects
 
     # The version of the win32-ipc library
-    VERSION = '0.6.2'
+    VERSION = '0.6.3'
 
     SIGNALED  = 1
     ABANDONED = -1
